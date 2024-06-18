aaa5504a5f22109867734a224086041e81f54577
Camille Bucatcat <camille.bucatcat@gmail.com>
2024-06-18 22:52:29 +0800

    update to svg

--- src/app/pages/scan-to-play/scan-to-play.page.ts
+++ src/app/pages/scan-to-play/scan-to-play.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  amount: number;
+  isSelected: boolean;
+}
 @Component({
   selector: 'app-scan-to-play',
   templateUrl: './scan-to-play.page.html',
@@ -7,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScanToPlayPage implements OnInit {
 
+  items: Item[] = [
+    { amount: 10, isSelected: false },
+    { amount: 20, isSelected: false },
+    { amount: 50, isSelected: false },
+    { amount: 100, isSelected: false },
+  ];
+
   constructor() { }
+
+  toggleSelection(selectedItem: Item) {
+    // Deselect all items first
+    this.items.forEach(item => {
+      if (item !== selectedItem) {
+        item.isSelected = false;
+      }
+    });
+
+    // Toggle the selected state of the clicked item
+    selectedItem.isSelected = !selectedItem.isSelected;
+  }
 
   ngOnInit() {
   }
